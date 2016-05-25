dea80962f16f0b895de1c49aa87b6240a90e9cb3
yolixtly <yolixtly_13@hotmail.com>
2016-05-25 11:55:34 -0400

    search is limited to youtube#video type results

--- apps/app.js
+++ apps/app.js
@@ -13,7 +13,7 @@ $(function(){
       part:'snippet',
       key: 'AIzaSyCQQNC20CuEMQVuJSe-NVyZ9NXZHUbWlgc',
       q: searchTerm,
-      maxResults: 9,
+      maxResults: 9
     };
      url = 'https://www.googleapis.com/youtube/v3/search';
 
@@ -26,10 +26,12 @@ $(function(){
     function showResults(results) {
       var html = "";
       $.each(results, function(index,value){
+        if(value.id.kind == "youtube#video"){
         html += '<div class="col-xs-12 col-md-4"><div class="video-display"><p>' + value.snippet.title + 
         '</p> <a href="https://www.youtube.com/watch?v=' + value.id.videoId + '"><img class="thumbnails" src="' + 
         value.snippet.thumbnails.high.url + '"/></a></div></div>';
-      });
+      } 
+    });
       $('#search-results').append(html);
     };
 });
